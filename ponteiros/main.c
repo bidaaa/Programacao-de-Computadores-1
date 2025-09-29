50991bd7c3881134da158d52c995eea4a727a732
bidaaa <bidaviniciusribas@gmail.com>
2025-09-29 19:30:47 -0300

    terminada a lista de ponteiros do dia 22/09

--- ponteiros/main.c
+++ ponteiros/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 void exercicio1(){
     int a = 0, b;
@@ -29,13 +31,40 @@ void swap(int *x, int *y){
     printf("Depois da funcao swap: x = %d e y = %d\n", *x, *y);
 }
 
+void exercicio3(char *s) {
+    if (s == NULL || *s == '\0') {
+        return;
+    }
+
+    char *inicio = s;
+    char *fim = s + strlen(s) - 1;
+    char temp;
+
+    while (inicio < fim) {
+        temp = *inicio;
+        *inicio = *fim;
+        *fim = temp;
+
+        inicio++;
+        fim--;
+    }
+}
+
+void exercicio4(int *a, int b) {
+    *a = *a + b;
+}
+
+void exercicio5(float R, float *area, float *volume) {
+    *area = 4 * 3.14 * (R * R);
+    *volume = (4.0 / 3.0) * 3.14 * (R * R * R);
+}
 
 int main(){
 
     /*
     exercicio1();
-    */
-   //exercicio 2
+    
+    //Exercicio 2
     printf("Define dois valores inteiros: ");
     int a, b;
     scanf("%d %d", &a, &b);
@@ -43,6 +72,42 @@ int main(){
     swap(&a,&b);
 
 
+    Exercicio 3
+    char string[200];
+
+    printf("Digite uma string (ate 200 caracteres): ");
+
+    if (fgets(string, sizeof(string), stdin)) {
+    string[strcspn(string, "\n")] = '\0';
+
+    printf("Original: \"%s\"\n", string);
+
+    exercicio3(string);
+
+    printf("Invertida: \"%s\"\n", string);
+    } else {
+    printf("Erro ao ler a entrada.\n");
+    }
+
+
+    Exercicio 4
+    int a, b;
+    printf("Digite dois valores inteiros: ");
+    scanf("%d %d", &a, &b);
+    printf("Antes da funcao: a = %d e b = %d\n", a, b);
+    exercicio4(&a, b);
+    printf("Depois da funcao: a = %d e b = %d\n", a, b);
+
+    Exercicio 5
+    float R, area, volume;
+    printf("Digite o valor do raio: ");
+    scanf("%f", &R);
+
+    exercicio5(R, &area, &volume);
+
+    printf("Area da esfera: %.2f\n", area);
+    printf("Volume da esfera: %.2f\n", volume);
+    */
     
 return 0;
 }
