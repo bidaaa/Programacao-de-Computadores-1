e9b00c36b3ad18140f99a38ae8e24d05ecc82f80
bidaaa <bidaviniciusribas@gmail.com>
2025-10-06 19:10:10 -0300

    Subindo a lista 3 pro GitHub

---
+++ ponteiros3/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+Lista de Exercícios - Ponteiros 2
+Autor: Vinicius Ribas Bida
+RA: 25070323
+Data: 30/09/2025
+*/
+
+int main(){
+
+
+    
+return 0;
+}
