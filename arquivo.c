6c15d0be35f4dd7cba4c76b8028c9fd331079b50
bidaaa <bidaviniciusribas@gmail.com>
2025-10-20 20:11:37 -0300

    Mais testes e funções

--- arquivo.c
+++ arquivo.c
@@ -15,20 +15,55 @@ int main(){
     // return 0;
 
 
+    // FILE *arquivo;
+    // char string[100];
+    // int i;
+    // arquivo = fopen("arquivo.txt", "w");
+    // if(arquivo == NULL){
+    //     printf("Erro ao abrir o arquivo.\n");
+    //     system("pause");
+    //     exit(1);
+    // }
+    // printf("Digite uma string: ");
+    // gets(string);
+    // for(i=0; i<strlen(string); i++){
+    //     fputc(string[i], arquivo);
+    // }
+    // fclose(arquivo);
+    // system("pause");
+    // return 0;
+
+
+    // FILE *arquivo;
+    // char c;
+    // arquivo = fopen("arquivo.txt", "r");
+    // if(arquivo == NULL){
+    //     printf("Erro ao abrir o arquivo.\n");
+    //     system("pause");
+    //     exit(1);
+    // }
+    // int i;
+    // for(i=0; i<50; i++){
+    //     c = fgetc(arquivo);
+    //     printf("%c", c);
+    // }
+    // fclose(arquivo);
+    // system("pause");
+    // return 0;
+
     FILE *arquivo;
-    char string[100];
-    int i;
-    arquivo = fopen("arquivo.txt", "w");
+    char c;
+    arquivo = fopen("arquivo.txt", "r");
     if(arquivo == NULL){
         printf("Erro ao abrir o arquivo.\n");
         system("pause");
         exit(1);
     }
-    printf("Digite uma string: ");
-    gets(string);
-    for(i=0; i<strlen(string); i++){
-        fputc(string[i], arquivo);
+
+    while((c = fgetc(arquivo)) != EOF){
+        printf("%c", c);
     }
+    printf("\n");
     fclose(arquivo);
     system("pause");
     return 0;
