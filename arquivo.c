d73cdd10c5a3e40e40dd60da54e09e1808b1bf95
bidaaa <bidaviniciusribas@gmail.com>
2025-10-20 19:51:25 -0300

    Teste de escrita e leitura conjuntas

--- arquivo.c
+++ arquivo.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
-    FILE *arquivo;
-    arquivo = fopen("exemplo.bin", "wb");
-    if(arquivo == NULL){
-        printf("Erro ao abrir.\n");
-        return 1;
-    }
+    // FILE *arquivo;
+    // arquivo = fopen("exemplo.bin", "wb");
+    // if(arquivo == NULL){
+    //     printf("Erro ao abrir.\n");
+    //     return 1;
+    // }
 
+    // fclose(arquivo);
+    // system("pause");
+    // return 0;
+
+
+    FILE *arquivo;
+    char string[100];
+    int i;
+    arquivo = fopen("arquivo.txt", "w");
+    if(arquivo == NULL){
+        printf("Erro ao abrir o arquivo.\n");
+        system("pause");
+        exit(1);
+    }
+    printf("Digite uma string: ");
+    gets(string);
+    for(i=0; i<strlen(string); i++){
+        fputc(string[i], arquivo);
+    }
     fclose(arquivo);
     system("pause");
     return 0;
-}
+ }
