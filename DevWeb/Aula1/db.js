0b3433b1f50d0db6c1cac9d9f094ef6fa2e26b14
bidavinicius <bidaviniciusribas@gmail.com>
2026-08-03 16:01:44 -0300

    adicionando crud aula 1

---
+++ DevWeb/Aula1/db.js
@@ -0,0 +1,20 @@
+const agenda = [
+  {
+    id: 1,
+    nome: 'João',
+    telefone: '123456789',
+    email: 'joao@email.com',
+    nota: "caba foda",
+    ativo: true
+    },
+    {
+    id: 2,
+    nome: 'Maria',
+    telefone: '987654321',
+    email: 'maria@email.com',
+    nota: "derik",
+    ativo: true
+    }
+];
+
+export default agenda;
---
+++ DevWeb/Aula1/server.js
@@ -0,0 +1,44 @@
+import agenda from './db.js';
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send(agenda);
+});
+
+app.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const contato = agenda.find(c => c.id === id);
+
+  if (contato) {
+    res.status(200).send(contato);
+  } else {
+    res.status(404).send({ message:'Contato não encontrado'});
+  }
+});
+
+app.post('/', (req, res) => {
+  const { nome, telefone, email, nota, ativo } = req.body;
+  agenda.push({ id: agenda.length + 1, nome, telefone, email, nota, ativo });
+  res.status(201).send({ message: 'Contato adicionado com sucesso' });
+});
+
+app.delete('/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const indice = agenda.findIndex(c => c.id === id)
+
+    if(indice === -1){
+        res.status(404).send({message: "Contato não encontrado"})
+    }
+
+    const contatoRemovido = agenda.splice(indice,1)
+    res.status(200).send("Contato deletado com sucesso")
+})
+
+app.listen(port, () => {
+  console.log("Server ligado");
+});
